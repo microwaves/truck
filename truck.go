e1d2bb6c9d6262a5b17c5577984994bd2a87e5da
Stephano Zanzin <stephano.zanzin@signavio.com>
2017-10-13 18:06:33 +0200

    first commit

---
+++ truck.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+)
+
+var (
+	target string
+	port   int
+)
+
+func init() {
+	flag.StringVar(&target, "target", "", "<host>:<port>")
+	flag.IntVar(&port, "port", 4444, "port")
+}
+
+func main() {
+	signals := make(chan os.Signal, 1)
+	stop := make(chan bool)
+	signal.Notify(signals, os.Interrupt)
+
+	go func() {
+		for _ = range signals {
+			fmt.Println("\nCtrl-C received, stopping...")
+			stop <- true
+		}
+	}()
+
+	incoming, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("Could not listen on %d: %v", port, err)
+	}
+	fmt.Printf("Listen on %d\n", port)
+
+	client, err := incoming.Accept()
+	if err != nil {
+		log.Fatal("Could not accept connection", err)
+	}
+	defer client.Close()
+	fmt.Printf("Client %v successfully connected!\n", client.RemoteAddr())
+
+	targetConn, err := net.Dial("udp", target)
+	if err != nil {
+		log.Fatal("Could not connect to target", err)
+	}
+	defer targetConn.Close()
+	fmt.Printf("Connection to %v established!\n", targetConn.RemoteAddr())
+
+	go func() { io.Copy(targetConn, client) }()
+	go func() { io.Copy(client, targetConn) }()
+
+	<-stop
+}
